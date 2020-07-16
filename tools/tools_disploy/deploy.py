6678ded667550f17db088e1884540e1f6fd5a27d
小桀 <812797569@qq.com>
2020-07-16 13:45:35 +0800

    auto commit

--- tools/tools_disploy/deploy.py
+++ tools/tools_disploy/deploy.py
@@ -1,86 +1,34 @@
 #!/usr/bin/python3
-import glob
-
 import deploy_config
+from deployments import Deployments
+import glob
 import os
 import git
 import subprocess
 import paramiko
 
 
-class NoKeyPathException(Exception): pass
-
-
-class PackageFailedException(Exception): pass
-
-
-class NoJarException(Exception): pass
-
-
-def git_update(service_name, source_root, git_url, git_branch):
-    source_path = os.path.join(source_root, service_name)
-    if os.path.isdir(source_path):
-        # git pull
-        repo = git.Repo(source_path)
-        repo.remote().pull()
-        pass
-    else:
-        repo = git.Repo.clone_from(git_url, source_path)
-        repo.git.checkout(git_branch)
-        # git clone
-        pass
-
-
-def maven_package(service_name, source_root):
-    source_path = os.path.join(source_root, service_name)
-    if not os.path.isdir(source_path):
-        raise NoKeyPathException("source_path not exist.")
-    os.chdir(source_path)
-    pipe = subprocess.Popen('mvn package -D maven.test.skip=True', shell=True, stdout=subprocess.PIPE)
-
-    for line in pipe.stdout.readlines():
-        print(line)
-
-    pipe.wait()
-    if pipe.returncode != 0:
-        raise PackageFailedException
-
-
 def send_file(ip, machine_user, machine_password, sour_file_path, target_file_dir):
-    transport = paramiko.Transport(ip)
-    transport.connect(username=machine_user, password=machine_password)
-    sftp = paramiko.SFTPClient.from_transport(transport)
-    print(sour_file_path)
-    print(target_file_dir)
-    sftp.put(sour_file_path, target_file_dir)
 
 
-def send_jar_to_target(service_name, source_root, target_path, target_ip, machine_user, machine_password):
-    source_path = os.path.join(source_root, service_name)
-    jar_path = os.path.join(source_path, target_path)
-
-    os.chdir(jar_path)
-    jars = glob.glob('*.jar')
-    if len(jars) == 0:
-        raise NoJarException('no jar in target path.')
-
-    jar_file_path = os.path.join(jar_path, jars[0])
-    print(jar_file_path)
-    send_file(target_ip, machine_user, machine_password, jar_file_path, "/home/mmpprd/tmp/temp.jar")
+# def send_jar_to_target(service_name, source_root, target_path, target_ip, machine_user, machine_password):
+#     source_path = os.path.join(source_root, service_name)
+#     jar_path = os.path.join(source_path, target_path)
+#
+#     os.chdir(jar_path)
+#     jars = glob.glob('*.jar')
+#     if len(jars) == 0:
+#         raise NoJarException('no jar in target path.')
+#
+#     jar_file_path = os.path.join(jar_path, jars[0])
+#     print(jar_file_path)
+#     send_file(target_ip, machine_user, machine_password, jar_file_path, "/home/mmpprd/tmp/temp.jar")
 
 
 if __name__ == '__main__':
     service_name = 'online-bb-member'
-    source_root = "/home/mmpprd/source"
     service = deploy_config.services[service_name]
 
-    git_update(service_name, source_root,
-               service.git_config.git_url,
-               service.git_config.git_branch)
-    maven_package(service_name, source_root)
-
-    send_jar_to_target(service_name, source_root,
-                       service.git_config.jar_path,
-                       service.targets_config[0].ip,
-                       service.targets_config[0].machine_user,
-                       service.targets_config[0].machine_password)
+    deployments = Deployments(service)
+    deployments.pull()
+    deployments.package()
---
+++ tools/tools_disploy/deployments.py
@@ -0,0 +1,96 @@
+import os
+import subprocess
+
+import git
+import paramiko
+
+
+class NoKeyPathException(Exception): pass
+
+
+class PackageFailedException(Exception): pass
+
+
+class NoJarException(Exception): pass
+
+
+class SSHUtils:
+    def __init__(self, ip, machine_user, machine_password):
+        self.transport = paramiko.Transport(ip)
+        self.transport.connect(username=machine_user, password=machine_password)
+
+    def send_file(self, source_file, target_file):
+        sftp = paramiko.SFTPClient.from_transport(self.transport)
+        print(source_file)
+        print(target_file)
+        sftp.put(source_file, target_file)
+
+        # todo 这个地方需要关闭么
+
+
+class Deployment:
+    def __init__(self, source_root, service, target_config):
+        self.service_name = service.service_name
+        self.source_root = source_root
+        self.source_path = os.path.join(source_root, service.service_name)
+        self.jar_path = os.path.join(self.source_path, service.git_config.jar_path)
+
+        # jar 包将会被发送到这个目录下
+        self.target_dir = 'deploy/%s_%s' % (self.service_name, target_config.port)
+
+        self.transportUtil = SSHUtils(target_config.ip,
+                                      target_config.machine_user,
+                                      target_config.machine_password)
+        pass
+
+    def send_jar(self):
+        if not os.path.isfile(self.jar_path):
+            raise NoJarException('Jar Not Exist.')
+        self.transportUtil.send_file(self.jar_path, self.target_dir)
+        pass
+
+    def deploy(self):
+        pass
+
+    def kill(self):
+        pass
+
+
+class Deployments:
+    def __init__(self, source_root, service):
+        self.service_name = service.service_name
+        self.source_root = source_root
+        self.source_path = os.path.join(source_root, service.service_name)
+
+        self.git_url = service.git_config.git_url
+        self.git_branch = service.git_config.git_brnch
+
+        self.deployments = []
+
+        for target_config in service.targets_config:
+            self.deployments.append(Deployment(source_root, service, target_config))
+
+    def package(self):
+        if not os.path.isdir(self.source_path):
+            raise NoKeyPathException("source_path not exist.")
+        os.chdir(self.source_path)
+        pipe = subprocess.Popen('mvn package -D maven.test.skip=True', shell=True, stdout=subprocess.PIPE)
+
+        for line in pipe.stdout.readlines():
+            print(line)
+
+        pipe.wait()
+        if pipe.returncode != 0:
+            raise PackageFailedException
+
+    def pull(self):
+        if os.path.isdir(self.source_path):
+            # git pull
+            repo = git.Repo(self.source_path)
+            repo.remote().pull()
+            pass
+        else:
+            repo = git.Repo.clone_from(self.git_url, self.source_path)
+            repo.git.checkout(self.git_branch)
+            # git clone
+            pass
