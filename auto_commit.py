2d546ad34c04aeedb1729c752c500036cc47fe3b
小桀 <812797569@qq.com>
2020-07-17 21:42:17 +0800

    auto commit

--- auto_commit.py
+++ auto_commit.py
@@ -5,6 +5,9 @@ if repo.is_dirty():
     git = repo.git
     git.add('.')
     git.commit('-m', "auto commit")
+
+    remote = repo.remote()
+    remote.push()
 else:
     print('noting to commit.')
 input()
