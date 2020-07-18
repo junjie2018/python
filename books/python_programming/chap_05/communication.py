be401c785a939ccb2a68137d0904437b75360ee2
小桀 <812797569@qq.com>
2020-07-18 18:42:44 +0800

    auto commit

--- books/python_programming/chap_05/communication.py
+++ books/python_programming/chap_05/communication.py
@@ -22,5 +22,27 @@ def parent():
             print('Parent %d got [%s] at %s' % (os.getpid(), line, time.time()))
 
 
+def child2(pipeout):
+    zzz = 0
+    while True:
+        time.sleep(zzz)
+        msg = ('Spam %03d\n' % zzz).encode()
+        os.write(pipeout, msg)
+        zzz = (zzz + 1) % 5
+
+
+def parent2():
+    pipein, pipeout = os.pipe()
+    if os.fork() == 0:
+        os.close(pipein)
+        child2(pipeout)
+    else:
+        os.close(pipeout)
+        pipein = os.fdopen(pipein)
+        while True:
+            line = pipein.readline()[:-1]
+            print('Parent %d got [%s] at %s' % (os.getpid(), line, time.time()))
+
+
 if __name__ == '__main__':
     eval(sys.argv[1])()
