f1b10d96de0e76e16f0b82ff01705d5549490abd
小桀 <812797569@qq.com>
2020-07-20 08:43:31 +0800

    auto commit

--- books/python_programming/chap_05/communication.py
+++ books/python_programming/chap_05/communication.py
@@ -67,6 +67,48 @@ def parent3_main():
     parent3(pipein)
 
 
+def spawn(prog, *args):
+    stdinFd = sys.stdin.fileno()
+    stdoutFd = sys.stdout.fileno()
+
+    parentStdin, childStdout = os.pipe()
+    childStdin, parentStdout = os.pipe()
+
+    pid = os.fork()
+    if pid:
+        os.close(childStdout)
+        os.close(childStdin)
+        os.dup2(parentStdin, stdinFd)
+        os.dup2(parentStdout, stdoutFd)
+    else:
+        os.close(parentStdin)
+        os.close(parentStdout)
+        os.dup2(childStdin, stdinFd)
+        os.dup2(childStdout, stdoutFd)
+        args = (prog,) + args
+        os.execvp(prog, args)
+        assert False, 'execvp failed!'
+
+
+def spawn_main():
+    """
+        os.dup2(fd1,fd2):把由文件描述符fd1命名的文件的所有相关系统信息复制到有fd2命名的文件中
+    :return:
+    """
+
+    mypid = os.getpid()
+    spawn('python', 'pipes-testchild.py', 'spam')
+
+    print('Hello 1 from parent', mypid)
+    sys.stdout.flush()
+    reply = input()
+    sys.stderr.write('Parent got: "%s"\n' % reply)
+
+    print('Hello 2 from parent', mypid)
+    sys.stdout.flush()
+    reply = sys.stdin.readline()
+    sys.stderr.write('Parent got: "%s"\n' % reply[:-1])
+
+
 if __name__ == '__main__':
     eval(sys.argv[1])()
-
